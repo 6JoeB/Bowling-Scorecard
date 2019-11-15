560126d447d6233dc829f3a86239b7119ec02615
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-15 09:51:27 +0000

    expect score to start at 0 test written

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -11,4 +11,8 @@ describe("Bowling scorecard", function () {
     it('creates an instance of the scorecard', function () {
         expect(scorecard).toBeInstanceOf(Scorecard);
     });
+
+    it('starts with a score of 0', function () {
+        expect(scorecard.checkScore).toEqual(0)
+    });
 });
