8d31c12ab1be19a138ae951885ff88351026262a
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-15 10:47:19 +0000

    first frame scoring tests passed

--- src/scorecard.js
+++ src/scorecard.js
@@ -3,12 +3,34 @@
 function Scorecard() {
     this.totalScore = 0;
     this.currentFrameScore = 0;
-    this.currentFrame = 0;
+    this.currentFrame = 1;
     this.spare = false;
-    this.strike = true;
-    this.frameArray = [];
+    this.strike = false;
+    this.frameArray = ["placeholder"];
 };
 
-Scorecard.prototype.addScore = function (first, second) {
-    this.score
+Scorecard.prototype.addScore = function (firstShot, secondShot) {
+
+    this.frameArray.push(firstShot)
+    this.frameArray.push(secondShot) //adding the first and second bowls to the frame array
+    this.currentFrame += 1 //advancing the current frame value
+
+    this.currentFrameScore = (firstShot + secondShot) // adding the shot scores together to equal total frame score
+
+    if (this.strike === true) {
+        this.totalScore += this.currentFrameScore
+    } else if (this.spare === true) {
+        this.totalScore += firstShot
+    } //adding the additional scores from strikes or spares
+
+    this.totalScore += this.currentFrameScore //adds the score from this frame to the total
+
+    if (firstShot === 10) {
+        this.strike = true; //checking if the score was a strike
+    } else if (this.currentFrameScore === 10) {
+        this.spare = true; //checking if the score was a spare
+    } else {
+        this.spare = false;
+        this.strike = false; //resetting the spare and strike values to false
+    }
 };
